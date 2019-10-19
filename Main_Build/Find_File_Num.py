5bd501b3c71d11cceb582ab75abcb79fc392f594
Paul SaundersPaul Saunders <psaunder@ualberta.ca>
2019-10-19 16:43:04 -0600

    Web scraping to validate days

--- Main_Build/Find_File_Num.py
+++ Main_Build/Find_File_Num.py
@@ -5,45 +5,30 @@ Created on Sat Oct 19 12:57:57 2019
 @author: eredm
 """
 
+from json import load
+import requests
 from astropy.io import fits
 from astropy.utils.data import download_file
 import os
 
 base_url = "ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/ASTRO/"
-years = {'2017':{},'2018':{},'2019':{}}
-year_url = list(years.keys())
-year_list = year_url
-#print(list(years.keys()))
-for index in range(len(year_url)):
-    year_url[index] = base_url + str(year_url[index] + '/')
-    years[year_list] = [os.path.join(year_url[index],o) for o in os.listdir(year_url[index]) 
-                    if os.path.isdir(os.path.join(url[index],o))]
 
-[x for x in os.listdir('ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/ASTRO/') if os.path.isfile(x)] # only files
+year_urls = [base_url + year for year in ['2015/', '2019/']]
+days = [str.zfill(str(num), 3) for num in range(1, 366)]
+html_days = ["""<td sortable-data="1""" + day + """">""" for day in days]
 
+day_urls = []
+valid_urls = []
 
-d='.'
-[os.path.join(d, o) for o in os.listdir(d) 
-                    if os.path.isdir(os.path.join(d,o))]
+for year_url in year_urls:
+    for day in days:
+        day_urls.append(year_url + day + '/')
 
+for day_url in day_urls:
+    try:
+        requests.get(day_url)
+    except:
+        print("Failed:", day_url)
+    else:
+        valid_urls.append(day_url)
 
-
-
-for root, dirs, files in os.walk("."):
-    path = root.split(os.sep)
-    print((len(path) - 1) * '---', os.path.basename(root))
-    for file in files:
-        print(len(path) * '---', file)
-
-image_file = download_file('http://data.astropy.org/tutorials/FITS-images/HorseHead.fits', cache=True )
-
-
-[x for x in os.listdir('.') if os.path.isfile(x)] # only files
-
-
-
-
-
-    
-    
-  
