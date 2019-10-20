29820f44f0342920f20ca0c96959773d45665799
Paul SaundersPaul Saunders <psaunder@ualberta.ca>
2019-10-19 20:15:01 -0600

    Import images as fits

--- Main_Build/Find_File_Num.py
+++ Main_Build/Find_File_Num.py
@@ -22,7 +22,6 @@ import sys
 
 base_url = "ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/ASTRO/"
 years = ['NESS/2015', '2019']
-placeholder = ['a;lsdkj', '3dlsaf']
 year_urls = [base_url + year + '/' for year in years]
 days = ['1' + str.zfill(str(num), 3) for num in range(1, 366)]
 html_days = ["""<td sortable-data="1""" + day + """">""" for day in days]
@@ -30,36 +29,11 @@ html_days = ["""<td sortable-data="1""" + day + """">""" for day in days]
 image_urls = []
 
 def get_meta_data(image_list):
-    codeList = ['cp037', 'cp273', 'cp424', 'cp437', 'cp500', 'cp720', 
-    'cp737', 'cp775', 'cp850', 'cp852', 'cp855', 'cp856', 'cp857', 
-    'cp858', 'cp860', 'cp861', 'cp862', 'cp863', 'cp864', 'cp865', 
-    'cp866', 'cp869', 'cp875', 'cp949', 'cp1006', 'cp1026', 'cp1125', 
-    'cp1140', 'cp1250', 'cp1251', 'cp1252', 'cp1253', 'cp1254', 
-    'cp1255', 'cp1256', 'cp1257', 'cp1258', 'gbk', 'gb18030', 'latin_1', 
-    'iso8859_2', 'iso8859_3', 'iso8859_4', 'iso8859_5', 'iso8859_6', 
-    'iso8859_7', 'iso8859_8', 'iso8859_9', 'iso8859_10', 'iso8859_11', 
-    'iso8859_13', 'iso8859_14', 'iso8859_15', 'iso8859_16', 'koi8_r', 
-    'koi8_t', 'koi8_u', 'kz1048', 'mac_cyrillic', 'mac_greek', 'mac_iceland', 
-    'mac_latin2', 'mac_roman', 'mac_turkish', 'ptcp154', 'shift_jis_2004', 'shift_jisx0213']
-
     for url in image_list:
-        url = urllib.request.urlopen(url)
-        data = url.read()
-        # print(str(data[34559:34562]))
-        data = str(data).split('                                                                                                                                                                                               ')[0]
-        newData = str(data[:33600]).split('/')
-        print(newData[-1])
-        # for datapoint in newData:
-        #     print(datapoint)
-        # print(str(data[:5000]).split('                  '))
-        # for code in codeList:
-        #     try:
-        #         data = data.decode(code)
-        #     except:
-        #         pass
-        #     else:
-        #         print("Successful decoding! Code: {}".format(code))
-        # data = data.decode("cp037")
+        data = fits.open(url)
+        data = data[0].header
+        print(data.values)
+        sys.exit(1)
 
 counter = 0
 start = time()
