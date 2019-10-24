04df455d296900ad2d44e6d92c003a465728ff1d
Paul SaundersPaul Saunders <psaunder@ualberta.ca>
2019-10-24 09:15:35 -0600

    Commit before pull

--- Main_Build/Main_Build/settings.py
+++ Main_Build/Main_Build/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'Main_Build.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'images.db'),
+        'DATABASE_ENGINE': 'django.db.backends.sqlite3',
+        'DATABASE_NAME': os.path.join(BASE_DIR, 'images.db'),
     }
 }
 
